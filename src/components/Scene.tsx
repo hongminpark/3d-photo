98fe6738caf0d39c7d0ba6db3c4340253696f7a3
hongminpark <zhdals1026@gmail.com>
2023-12-22 19:41:57 +0900

    Initial Commit

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,123 +1,13 @@
-// @ts-nocheck
-import { animated, useSpring } from "@react-spring/three";
 import {
-    Environment,
     OrbitControls,
     OrthographicCamera,
-    Plane,
-    useGLTF,
+    PivotControls,
+    useAspect,
 } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
-import { Box3, Vector3 } from "three";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
+import { TextureLoader } from "three";
 import Loader from "./Loader";
-
-// Ground : y=0, Wall : z=0
-function VitsoeWallMounted({ position = [0, 0, 0] }) {
-    const gltf = useGLTF("/vitsoe/wall_mounted.glb", true);
-    const box = new Box3().setFromObject(gltf.scene);
-
-    // Attach to the ground
-    const offset = new Vector3(0, -box.min.y, 0.07);
-    const clonedScene = gltf.scene.clone();
-    clonedScene.position.set(...position);
-    clonedScene.position.add(offset);
-    clonedScene.traverse((child) => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    });
-    return <primitive object={clonedScene} />;
-}
-
-function VitsoeSemiWallMounted({ position = [0, 0, 0] }) {
-    const gltf = useGLTF("/vitsoe/semi_wall_mounted.glb", true);
-    const box = new Box3().setFromObject(gltf.scene);
-
-    // Attach to the ground
-    const offset = new Vector3(0, -box.min.y, 0);
-    const clonedScene = gltf.scene.clone();
-    clonedScene.position.set(...position);
-    clonedScene.position.add(offset);
-    clonedScene.traverse((child) => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    });
-    return <primitive object={clonedScene} />;
-}
-
-function VitsoeCompressed({ position = [0, 0, 0] }) {
-    const gltf = useGLTF("/vitsoe/compressed.glb", true);
-    const box = new Box3().setFromObject(gltf.scene);
-
-    // Attach to the ground
-    const offset = new Vector3(0, -box.min.y, 0.06);
-    const clonedScene = gltf.scene.clone();
-    clonedScene.position.set(...position);
-    clonedScene.position.add(offset);
-    clonedScene.traverse((child) => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    });
-    return <primitive object={clonedScene} />;
-}
-
-function VitsoeShelf({ position = [0, 1, 0] }) {
-    const gltf = useGLTF("/vitsoe/shelf.glb", true);
-    const clonedScene = gltf.scene.clone();
-    const box = new Box3().setFromObject(clonedScene);
-    const offsetToSemiWallMountedPostOffset = 0.08;
-    const offsetToWall = -box.min.z;
-    // FIXME : set as -box.min.y + (offsetToGround)
-    const offsetMiscAdjust = 0.3; // offset for assemble line
-    const offset = new Vector3(
-        0,
-        offsetMiscAdjust,
-        offsetToWall + offsetToSemiWallMountedPostOffset
-    );
-
-    // Attach to the wall
-    clonedScene.position.set(...position);
-    clonedScene.position.add(offset);
-    clonedScene.traverse((child) => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    });
-    return <primitive object={clonedScene} />;
-}
-
-function VitsoeInclinedShelf({ position = [0, 1, 0] }) {
-    const gltf = useGLTF("/vitsoe/inclined_shelf.glb", true);
-    const clonedScene = gltf.scene.clone();
-    const box = new Box3().setFromObject(clonedScene);
-    const offsetToSemiWallMountedPostOffset = 0.08;
-    const offsetToWall = -box.min.z;
-    // FIXME : set as -box.min.y + (offsetToGround)
-    const offsetMiscAdjust = 0.3; // offset for assemble line
-    const offset = new Vector3(
-        0,
-        offsetMiscAdjust,
-        offsetToWall + offsetToSemiWallMountedPostOffset
-    );
-
-    // Attach to the wall
-    clonedScene.position.set(...position);
-    clonedScene.position.add(offset);
-    clonedScene.traverse((child) => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    });
-    return <primitive object={clonedScene} />;
-}
 
 export default function Scene({
     postModel,
@@ -127,17 +17,9 @@ export default function Scene({
     distance,
     setDistance,
 }) {
-    const AnimatedVitsoeSemiWallMounted = animated(VitsoeSemiWallMounted);
-    const AnimatedVitsoeWallMounted = animated(VitsoeWallMounted);
-    const AnimatedVitsoeCompressed = animated(VitsoeCompressed);
-    const { leftPosition, rightPosition } = useSpring({
-        leftPosition: [-distance / 2, 0, 0],
-        rightPosition: [distance / 2, 0, 0],
-        config: { tension: 170, friction: 26 },
-    });
-
     const cameraCenter = [0, 1.5, 0];
     const ref = useRef();
+    const [isDragging, setIsDragging] = useState(false);
     return (
         <div
             ref={ref}
@@ -152,58 +34,95 @@ export default function Scene({
             <Canvas shadows>
                 <OrthographicCamera
                     makeDefault
-                    position={[5, 3, 5]}
+                    position={[0, 3, 5]}
                     zoom={200}
                     onUpdate={(self) => self.lookAt(...cameraCenter)}
                 />
-                <color attach="background" args={["#F2F2F2"]} />
-                <Environment files="/keyshot.hdr" />
+                <color attach="background" args={["#ffffff"]} />
                 <Suspense fallback={<Loader />}></Suspense>
-                <OrbitControls target={cameraCenter} />
-                {postModel === "SemiWallMounted" && (
-                    <>
-                        <AnimatedVitsoeSemiWallMounted
-                            position={leftPosition}
-                        />
-                        <AnimatedVitsoeSemiWallMounted
-                            position={rightPosition}
-                        />
-                    </>
-                )}
-                {postModel === "WallMounted" && (
-                    <>
-                        <AnimatedVitsoeWallMounted position={leftPosition} />
-                        <AnimatedVitsoeWallMounted position={rightPosition} />
-                    </>
-                )}
-                {postModel === "Compressed" && (
-                    <>
-                        <AnimatedVitsoeCompressed position={leftPosition} />
-                        <AnimatedVitsoeCompressed position={rightPosition} />
-                    </>
-                )}
-                {shelfModel === "Shelf" && <VitsoeShelf />}
-                {shelfModel === "InclinedShelf" && <VitsoeInclinedShelf />}
-
-                <Plane
-                    args={[5, 5]}
-                    rotation={[-Math.PI / 2, 0, 0]}
-                    position={[0, 0, 0]}
-                    receiveShadow
-                    castShadow
-                >
-                    <meshStandardMaterial color={"white"} />
-                </Plane>
-                <directionalLight
-                    castShadow
-                    position={[0, 6, 5]} // Adjust the position to be closer to the objects
-                    intensity={2} // Increase intensity for darker shadows
-                    shadow-mapSize-width={1024} // Higher resolution for better shadow quality
-                    shadow-mapSize-height={1024}
-                    // shadow-radius={0} // Sharper shadows
-                    shadow-bias={-0.0001} // Adjust bias to avoid artefacts
+                {!isDragging && <OrbitControls target={cameraCenter} />}
+                <MyImageComponent
+                    url="/5.png"
+                    position={[0, 1.6, 0]}
+                    scale={1}
+                    setIsDragging={setIsDragging}
+                    renderOrder={2}
+                />
+                <MyImageComponent
+                    url="/4.png"
+                    position={[0, 1, 0]}
+                    scale={1}
+                    setIsDragging={setIsDragging}
+                    renderOrder={1}
+                />
+                <MyImageComponent
+                    url="/6.png"
+                    position={[0, 0.4, 0]}
+                    scale={1.15}
+                    setIsDragging={setIsDragging}
+                    renderOrder={0}
                 />
             </Canvas>
         </div>
     );
 }
+
+const MyImageComponent = ({
+    url,
+    position,
+    scale = 1,
+    setIsDragging,
+    renderOrder,
+}) => {
+    const texture = useLoader(TextureLoader, url);
+    const [imageWidth, imageHeight] = useAspect(
+        texture.image.width,
+        texture.image.height
+    );
+    const aspectScale = [
+        (imageWidth / 10) * scale,
+        (imageHeight / 10) * scale,
+        1,
+    ];
+    const meshRef = useRef();
+    const pivotRef = useRef();
+    const [controlsVisible, setControlsVisible] = useState(false);
+
+    // Event handler for click
+    const onImageClick = (e) => {
+        e.stopPropagation();
+        setControlsVisible(!controlsVisible); // Toggle visibility of PivotControls
+    };
+
+    return (
+        <>
+            <PivotControls
+                ref={pivotRef}
+                rotation={[0, -Math.PI / 2, Math.PI / 2]}
+                anchor={[1, -1, -1]}
+                scale={75}
+                depthTest={false}
+                fixed
+                lineWidth={2}
+                onDragStart={() => setIsDragging(true)}
+                onDragEnd={() => setIsDragging(false)}
+                visible={controlsVisible}
+            >
+                <mesh
+                    ref={meshRef}
+                    position={position}
+                    scale={aspectScale}
+                    onClick={onImageClick}
+                    renderOrder={renderOrder}
+                >
+                    <planeGeometry args={[1, 1]} />
+                    <meshBasicMaterial
+                        map={texture}
+                        transparent={true}
+                        depthTest={false}
+                    />
+                </mesh>
+            </PivotControls>
+        </>
+    );
+};
